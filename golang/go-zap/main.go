cb5c2e88436348e8c6fef664d69847e2ed79b189
董家宏 <dongjiahong@hotmail.com>
2021-01-12 10:03:30 +0800

    update zap

--- golang/go-zap/main.go
+++ golang/go-zap/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"zap-test/log"
+	"zap-test/log2"
 )
 
 func main() {
 	log.Logger.Info("info")
+	log2.Logger.Info("this is info")
+	log2.Logger.Debug("this is Debug")
+	log2.Logger.Error("this is error")
 }
--- golang/gorm/main.go
+++ golang/gorm/main.go
@@ -9,12 +9,15 @@ import (
 
 var db *gorm.DB
 
-type DistributeRecord struct {
-	Id             int     `json:"id"`
-	Coin           float64 `json:"coin"`
-	CreateTime     int64   `json:"create_time"`
-	Status         int     `json:"status"`
-	DistributeCoin float64 `json:"distribute_coin"`
+type Student struct {
+	Id   int
+	Name string
+}
+
+type Class struct {
+	Id        int
+	ClassName string
+	Students  []Student
 }
 
 func initDB() {
@@ -22,9 +25,10 @@ func initDB() {
 	password := ""
 	host := "localhost"
 	port := 3306
-	dbName := "db_zkyc_ex"
+	dbName := "db_test"
 	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return "tb_" + defaultTableName
+		return defaultTableName
+		//return "tb_" + defaultTableName
 	}
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
@@ -32,69 +36,17 @@ func initDB() {
 
 	db, _ = gorm.Open("mysql", dsn)
 	db.SingularTable(true)
+	db.LogMode(true)
 }
 
 func test_raw() {
-	type Num struct {
-		Num float64
-	}
-	var num Num
-	if err := db.Raw("select sum(coin) as num from tb_distribute_record").Scan(&num).Error; err != nil {
-		fmt.Println("========> ", err)
-	}
+	var c []Class
+	err := db.Where("id = 1").Find(&c)
+	fmt.Println(c, err.GetErrors())
 
-	var drs []*DistributeRecord
-	if err := db.Raw("select * from tb_distribute_record").Scan(&drs).Error; err != nil {
-		fmt.Println("=====>>>> ", err)
-	}
-	fmt.Println("=====>>>> ", drs)
-}
-
-func test_find() {
-
-	var drs []DistributeRecord
-	if err := db.Find(&drs).Error; err != nil {
-		fmt.Println("====> find err: ", err)
-	}
-	fmt.Println(drs)
-
-	type DistributeRecordV2 struct {
-		DistributeRecord
-	}
-	var drs2 []DistributeRecordV2
-	if err := db.Find(&drs2).Error; err != nil {
-		fmt.Println("====> find err: ", err)
-	}
-	fmt.Println(drs2)
-}
-
-func test_struct_raw() {
-	var drs []*DistributeRecord
-	if err := db.Raw("select id, coin from tb_distribute_record limit 2;").Scan(&drs).Error; err != nil {
-		//if err := db.Raw("select id, coin from tb_distribute_record limit 2;").Scan(&drs).Error; err != nil {
-		fmt.Println("===> struct err: ", err)
-	}
-	fmt.Printf("===> drs: %+v", drs[0])
-}
-
-func test_struct_raw_parameter(dr interface{}) {
-	drs, _ := dr.([]*DistributeRecord)
-	if err := db.Raw("select id, coin from tb_distribute_record limit 2;").Scan(&drs).Error; err != nil {
-		fmt.Println("===> struct err: ", err)
-	}
-	fmt.Printf("===> drs: %+v\n", drs[0])
-}
-
-func run_test_struct_raw_parameter() {
-	drs := []*DistributeRecord{}
-	test_struct_raw_parameter(drs)
-	fmt.Printf("===> drs: %+v\n", drs)
 }
 
 func main() {
 	initDB()
-	//test_raw()
-	//test_find()
-	//test_struct_raw()
-	run_test_struct_raw_parameter()
+	test_raw()
 }
