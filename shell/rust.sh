9fb46339833ebeab5804c20c3e8688c8d798f1dd
董家宏 <dongjiahong@hotmail.com>
2021-02-22 14:41:44 +0800

    update

---
+++ shell/rust.sh
@@ -0,0 +1,6 @@
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#
