9c69fe9773538b0ab78cd3d0fbb03a40188fe472
董家宏 <dongjiahong@hotmail.com>
2021-05-11 19:36:59 +0800

    add machinery code

--- golang/machinery/main.go
+++ golang/machinery/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"math/rand"
 	"time"
 
 	"github.com/RichardKnop/machinery/v1"
@@ -9,25 +11,29 @@ import (
 	"github.com/RichardKnop/machinery/v1/tasks"
 )
 
+var server *machinery.Server
+
 func Sum(args []int64) (int64, error) {
-	time.Sleep(time.Second * 10)
 	sum := int64(0)
 	for _, arg := range args {
 		sum += arg
 	}
 	return sum, nil
-	//return sum, errors.New("我说他错了")
 }
 
-func CallBack(args ...int64) (int64, error) {
-	sum := int64(1)
+func SumRetry(args []int64) (int64, error) {
+	if rand.Intn(1) == 0 {
+		return 0, errors.New("rand err")
+		//return 0, tasks.NewErrRetryTaskLater("rand err", 2*time.Second) // 出错并在2秒后执行
+	}
+	var sum int64 = 0
 	for _, arg := range args {
-		sum *= arg
+		sum += arg
 	}
 	return sum, nil
 }
 
-func main() {
+func initServer() {
 	// 1.读取文件配置
 	cnf, err := config.NewFromYaml("./config.yml", false)
 	if err != nil {
@@ -36,7 +42,7 @@ func main() {
 	}
 
 	// 2.启服务
-	server, err := machinery.NewServer(cnf)
+	server, err = machinery.NewServer(cnf)
 	if err != nil {
 		log.Println("start server failed ", err)
 		return
@@ -47,11 +53,10 @@ func main() {
 		log.Println("reg task sum failed ", err)
 		return
 	}
-	//	if err := server.RegisterTask("call", CallBack); err != nil {
-	//		log.Println("reg task  callback failed ", err)
-	//		return
-	//	}
-
+	if err := server.RegisterTask("sumRetry", SumRetry); err != nil {
+		log.Println("reg task sumRetry failed ", err)
+		return
+	}
 	// 4.启动worker
 	// 这里的1是限制goruntine的并发数
 	worker := server.NewWorker("asong", 1)
@@ -62,7 +67,10 @@ func main() {
 
 		}
 	}()
+}
 
+// Simple 简单的异步调用，这里加入了延迟执行功能
+func Simple() {
 	// signature签名，里面包含了调度需要的信息
 	// task signature, 通过singature实例传递给server实例来调度任务
 	signature := &tasks.Signature{
@@ -74,8 +82,11 @@ func main() {
 			},
 		},
 		RetryTimeout: 100, // 重试超时
-		RetryCount:   1,   // 重试次数
+		RetryCount:   3,   // 重试次数
 	}
+	// 延迟执行，可以不加则不延迟
+	eta := time.Now().UTC().Add(time.Second * 2) // 延迟2s执行
+	signature.ETA = &eta
 
 	asyncResult, err := server.SendTask(signature)
 	if err != nil {
@@ -88,4 +99,37 @@ func main() {
 	}
 	// HumanReadableResults这个方法可以处理反射值，获得最终结果
 	log.Printf("get res is %v\n", tasks.HumanReadableResults(res))
+}
+
+// SimpleRetry 简单的重试
+func SimpleRetry() {
+	signature := &tasks.Signature{
+		Name: "sumRetry",
+		Args: []tasks.Arg{
+			{
+				Type:  "[]int64",
+				Value: []int64{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			},
+		},
+		RetryTimeout: 2, // 重试超时
+		RetryCount:   3, // 重试次数
+	}
+
+	asyncResult, err := server.SendTask(signature)
+	if err != nil {
+		log.Fatalln("send task err: ", err)
+	}
+
+	res, err := asyncResult.Get(time.Duration(time.Second * 1))
+	if err != nil {
+		log.Fatalln("get result err: ", err)
+	}
+	// HumanReadableResults这个方法可以处理反射值，获得最终结果
+	log.Printf("get res is %v\n", tasks.HumanReadableResults(res))
+}
+
+func main() {
+	initServer()
+	Simple()
+	SimpleRetry()
 }
