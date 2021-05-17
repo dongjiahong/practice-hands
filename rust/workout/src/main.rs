06682ad2b2b2c82ea20c0638ecf06cf1793e3b9f
董家宏 <dongjiahong@hotmail.com>
2021-05-17 18:45:37 +0800

    add workout for rust

---
+++ rust/workout/src/main.rs
@@ -0,0 +1,53 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation, 
+            value: None,
+        }
+    }
+    fn value(&mut self, args: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(args);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    // 闭包以|开始，里面是他的参数，如果有两个参数可以是，|x,y|，后面得大括号是存放函数体的
+    let mut expensive_result = Cacher::new(|num|{
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+        }
+    }
+}
