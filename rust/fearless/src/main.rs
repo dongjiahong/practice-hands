448ea86c60db75e0ae2310da9573cb702de1bf75
董家宏 <dongjiahong@hotmail.com>
2021-05-18 23:46:43 +0800

    update

--- rust/fearless/src/main.rs
+++ rust/fearless/src/main.rs
@@ -1,4 +1,4 @@
-
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -14,12 +14,21 @@ fn main() {
         println!("hi number {} from main thread!", i);
         thread::sleep(Duration::from_millis(100));
     }
-
+    // join表示等待线程执行完
     handle.join().unwrap();
 
     let v = vec![1, 2, 3];
-    let handler = thread::spawn(||{
+    // move转移所有权，不然线程李访问不到v
+    let handler = thread::spawn(move ||{
         println!("here's a vector: {:?}", v);
     });
     handler.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move ||{
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
