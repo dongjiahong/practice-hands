c8c57ab514f2b4c3a5d1c573d91620cfc16b7582
董家宏 <dongjiahong@hotmail.com>
2021-06-01 09:59:38 +0800

    update

--- docker/gogs/start.sh
+++ docker/gogs/start.sh
@@ -1,4 +1,7 @@
-docker run -p 10022:22 -p 10080:3000 --name=gogs-test \
--e TZ="Asia/Shanghai" \
--v `pwd`/data:/data  \
--d gogs/gogs
+docker run \
+    -p 10022:22 \
+    -p 10800:3000 \
+    --name=gogs-test \
+    -e TZ="Asia/Shanghai" \
+    -v `pwd`/data:/data  \
+    -d gogs/gogs
